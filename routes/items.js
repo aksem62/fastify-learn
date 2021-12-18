f589ec54fe1498cca86ed6e8a3f18480bea0ea9a
Andrei Semenov <aksem62@gmail.com>
2021-12-18 22:25:44 +0300

    make routes

---
+++ routes/items.js
@@ -0,0 +1,4 @@
+function itemRoutes(fastify,options, done){
+
+    done( )
+}
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const items = require('./Items')
 fastify.get('/items', (req,reply) => {
     reply.send({items})
 })
+fastify.get('/items/:id', (req,reply) => {
+    const {id} = req.params
+    const item = items.find(item => item.id === id)
+    reply.send({item})
+})
 
 const start = async () => {
     try {
